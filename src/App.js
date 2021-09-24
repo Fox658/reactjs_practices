936b25ce37be6ca921ab0508f482f889bd39fcd9
Fox658 <j.c.alvarez658@gmail.com>
2021-09-24 16:12:40 -0400

    add StarWars Component, pseudo Code, another click event n using state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Item from './Component/MyItem';
+// import Item from './Component/MyItem';
+
+
+class StarWars extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      heigth: null,
+      homworld: null,
+      films: [],
+
+    }
+  }
+  getNewCharacter() {
+    console.log("Get new character from a button");
+    this.setState(
+      {
+        name: "Pablo hurt",
+        heigth: 176,
+        homworld: "Toto.in",
+        films: ["dadass","yolo 2077", "call of cien fuegos"],
+  
+      }
+    );
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>{this.state.name}</h1>
+      <p>Height cm: {this.state.heigth}</p>
+      <p>Homeworld: {this.state.homworld}</p>
+      <ul>
+        <li>{this.state.films}</li>
+      </ul>
+      <button 
+        type="button" 
+        onClick={() => this.getNewCharacter()} 
+        className="btn"
+      >
+        Randomize Character
+      </button>
+
+    </div>
+    );
+  }
+}
+// this is for run multiple times a component "MyItem in App function comoponent"
+// const listPeople = [
+//   "Juancho", 
+//   "Leticia",
+// ];
+// let id = 0;
+// let items = [];
+// for(let item of listPeople){
+//   id += 1;
+//   items.push(
+//     <Item key={id} name={item} /> 
+//   );
+// };
 
 function App() {
-  const listPeople = [
-    "Juancho", 
-    "Leticia",
-  ];
-  let id = 0;
-  let items = [];
-  for(let item of listPeople){
-    id += 1;
-    items.push(
-      <Item key={id} name={item} /> 
-    );
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {items}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StarWars />
       </header>
     </div>
   );
 }
+
 export default App;
